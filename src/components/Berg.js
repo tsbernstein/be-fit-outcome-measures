49b922b63a55541e9a864609f2a457fda39d15c5
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-04-21 23:55:26 -0500

    begin adding outcome measure components and filling with appropriate inputs and calculations for each one

---
+++ src/components/Berg.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Berg extends React.Component {
+    calculate = () => {
+
+    }
+
+    render() {
+        return(
+            <div>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+                <input type='text'/>
+            </div>
+        )
+    };
+};
+
+export default Berg;
