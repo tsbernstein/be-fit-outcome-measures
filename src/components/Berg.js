c316414a36a9118a8223784aca32a85202a315dd
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-04-26 23:55:11 -0500

    start building out Berg calculation functionality

--- src/components/Berg.js
+++ src/components/Berg.js
@@ -3,30 +3,38 @@ import React from "react";
 class Berg extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            total: 0
+        }
+
+        this.updateTotal = this.updateTotal.bind(this);
+    }
+
+    calculate() {
 
     }
 
-    calculate = () => {
+    updateTotal(e){
 
     }
 
     render() {
         return(
             <div>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
-                <input type='number'/>
+                <input type='number' defaultValue={0} onChange={this.updateTotal}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
+                <input type='number' defaultValue={0}/>
             </div>
         )
     };
