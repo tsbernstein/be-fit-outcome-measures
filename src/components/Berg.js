7a38ca7f2e921532aab3f65b8403e8511663f3a7
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-05-05 23:01:43 -0500

    add reset button. Work on functionality of reset button to clear radio buttons

--- src/components/Berg.js
+++ src/components/Berg.js
@@ -25,6 +25,29 @@ class Berg extends React.Component {
 
         this.updateInput = this.updateInput.bind(this);
         this.calculate = this.calculate.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    reset(e){
+        this.setState({            
+            inputs: {
+                0: 0,
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0,
+                6: 0,
+                7: 0,
+                8: 0,
+                9: 0,
+                10: 0,
+                11: 0,
+                12: 0,
+                13: 0
+            },
+            total: 0
+        })
     }
 
     calculate(e) {
@@ -44,10 +67,6 @@ class Berg extends React.Component {
             inputs[num] = parsedInput
         }
         this.setState({inputs: inputs})
-    }
-
-    handleFocus(e) {
-        e.target.select()
     }
 
     render() {
@@ -306,21 +325,9 @@ class Berg extends React.Component {
                     </label>
                 </div>
                 {/* <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(0, e)}/> */}
-                {/* <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(1, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(2, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(3, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(4, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(5, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(6, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(7, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(8, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(9, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(10, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(11, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(12, e)}/>
-                <input type='number' defaultValue={0} onClick={this.handleFocus} onChange={(e) => this.updateInput(13, e)}/> */}
                 <button>Calculate</button>
                 {(this.state.total === 0) ? null : <div>{this.state.total}</div>}
+                <button onClick={this.reset}>Reset</button>
             </form>
         )
     };
