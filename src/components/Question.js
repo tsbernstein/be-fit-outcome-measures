64ce982358d53c546f3860d89267ef5f1f8f225f
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-05-09 23:29:15 -0500

    add question component and start planning out how it will be implemented into survey componenets

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Question extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            null
+        )
+    }
+}
+
+export default Question;
