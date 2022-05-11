b44fed3dba7c2f25e52bcc4e484fe5142515325d
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-05-10 23:44:35 -0500

    add componentdidmount function to question component to dynamically render the number of answers to a question

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,11 +3,29 @@ import React from "react";
 class Question extends React.Component{
     constructor(props){
         super(props)
+        this.state = []
+    }
+
+    answers(){
+
+    }
+
+    componentDidMount(){
+        let answers = [];
+        for (let i = 0; i < this.props.answers; i++){
+            answers.push(
+                <input type='radio' value={i} name={`question${i}`}></input>
+            )
+        };
+        this.setState(answers);
     }
 
     render(){
+
         return(
-            null
+            <div>
+
+            </div>
         )
     }
 }
