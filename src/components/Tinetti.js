334ef1d8b9bbe30efc94a424215c18506e636e0b
Tyler Bernstein <tsbernstein2593@gmail.com>
2022-05-30 23:56:42 -0500

    create 2 sub components to handle both parts of the tinetti assessment called tinetti1 and tinetti2

--- src/components/Tinetti.js
+++ src/components/Tinetti.js
@@ -1,51 +1,13 @@
 import React from "react";
 import Question from "./Question";
+import Tinetii1 from "./Tinetti1";
+import Tinetii2 from "./Tinetti2";
 
 class Tinetii extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            inputs: {
-                0: 0,
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0,
-                5: 0,
-                6: 0,
-                7: 0,
-                8: 0,
-                9: 0,
-                10: 0
-            },
-            total: 0
         }
-
-        this.updateInput = this.updateInput.bind(this);
-        this.calculate = this.calculate.bind(this);
-        this.reset = this.reset.bind(this);
-    }
-
-    reset(e){
-        Array.from(document.querySelectorAll("input")).forEach(
-            input => (input.checked = false)
-        )
-        this.setState({            
-            inputs: {
-                0: 0,
-                1: 0,
-                2: 0,
-                3: 0,
-                4: 0,
-                5: 0,
-                6: 0,
-                7: 0,
-                8: 0,
-                9: 0,
-                10: 0
-            },
-            total: 0
-        })
     }
 
     calculate(e) {
@@ -69,21 +31,10 @@ class Tinetii extends React.Component{
 
     render() {
         return(
-            <form onSubmit={this.calculate}>
-                <Question numAnswers={2} min={0} name='question-1' label='Question 1' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-2' label='Question 2' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-3' label='Question 3' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-4' label='Question 4' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-5' label='Question 5' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-6' label='Question 6' updateInput={this.updateInput}/>
-                <Question numAnswers={2} min={0} name='question-7' label='Question 7' updateInput={this.updateInput}/>
-                <Question numAnswers={2} min={0} name='question-8' label='Question 8' updateInput={this.updateInput}/>
-                <Question numAnswers={2} min={0} name='question-9' label='Question 9' updateInput={this.updateInput}/>
-                <Question numAnswers={3} min={0} name='question-10' label='Question 10' updateInput={this.updateInput}/>
-                <button>Calculate</button>
-                {(this.state.total === 0) ? null : <div>{this.state.total}</div>}
-                <button onClick={this.reset}>Reset</button>
-            </form>
+            <div>
+                <Tinetii1/>
+                <Tinetii2/>
+            </div>
         )
     };
 }
